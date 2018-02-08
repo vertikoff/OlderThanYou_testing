8ffe8d4ce512d7f0d93eca7d6fdc6cb434b7ffb3
Cole Vertikoff <cole.vertikoff@gmail.com>
2018-02-08 15:28:54 -0500

    Adds return_sum funtion to number_manipulation.py

--- number_manipulation.py
+++ number_manipulation.py
@@ -1,4 +1,10 @@
 # @vertikoff create sum function here
+def return_sum(num_list):
+    sum = 0
+    for num in num_list:
+        sum += num
+
+    return(sum)
 
 # @jlongc12 create tuple min/max function here
 
