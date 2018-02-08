34630ed15a93a087f4093408f87982abcc468d7c
James Long <jc500@duke.edu>
2018-02-08 16:35:52 -0500

    add return_limits draft

--- number_manipulation.py
+++ number_manipulation.py
@@ -6,6 +6,11 @@ def return_sum(num_list):
 
     return(sum)
 
+
 # @jlongc12 create tuple min/max function here
+def return_limits(num_list):
+    limits = (min(num_list), max(num_list))
+    return limits
+
 
 # @mackenna95 create max difference function here
