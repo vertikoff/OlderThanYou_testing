6b856ef33e1db2d89162a79e24f0377e3d1ef577
Cole Vertikoff <cole.vertikoff@gmail.com>
2018-02-08 15:43:39 -0500

    Adds one more test case (all neg numbers) and defines tollerance in test function as a var

--- test_sum_function.py
+++ test_sum_function.py
@@ -4,9 +4,12 @@ def test_sum_function():
     case_two = return_sum([3, 7, -2])
     case_three = return_sum([-6, -90, -40, 26])
     case_four = return_sum([6.9, 3.2, -4])
+    case_five = return_sum([-3.2, -1.8, -9.36, -872])
 
     assert case_one == 5
     assert case_two == 8
     assert case_three == -110
     # vertikoff - setting tollerance threshold here
-    assert case_four - 6.1 < 0.00000001
+    tollerance = 0.00000001
+    assert case_four - 6.1 < tollerance
+    assert case_five + 886.36 < tollerance
