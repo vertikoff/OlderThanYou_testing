ea187afd9b0adff966ec0ed045a36e52c20700d3
Cole Vertikoff <cole.vertikoff@gmail.com>
2018-02-20 18:52:33 -0500

    Updates all testing scripts to handle the new Class structure

--- test_adjacent_diff_function.py
+++ test_adjacent_diff_function.py
@@ -1,12 +1,18 @@
 def test_max_adjacent():
     import pytest
-    from number_manipulation import list_max_adjacent
-    assert list_max_adjacent([1, 2]) == 1
-    assert list_max_adjacent([0, 1, 3, 4, 5, 6, 7, 8, 9, 10]) == 2
-    assert list_max_adjacent([1, 2, 3]) == 1
-    assert list_max_adjacent([1, 2, 2.5, 5]) == 2.5
-    assert list_max_adjacent([-1, 1, -2]) == 3
-    pytest.raises(TypeError, list_max_adjacent, ["str1", "str2"])
-    pytest.raises(ValueError, list_max_adjacent, [1])
-    pytest.raises(ValueError, list_max_adjacent, [])
+    from number_manipulation import NumberListManipulator
+    assert NumberListManipulator([1, 2]).max_adjacent == 1
+    assert NumberListManipulator([0, 1, 3, 4, 5, 6, 7, 8, 9, 10]).max_adjacent == 2
+    assert NumberListManipulator([1, 2, 3]).max_adjacent == 1
+    assert NumberListManipulator([1, 2, 2.5, 5]).max_adjacent == 2.5
+    assert NumberListManipulator([-1, 1, -2]).max_adjacent == 3
+    with pytest.raises(TypeError):
+        output = NumberListManipulator(["str1", "str2"])
+        output.max_adjacent
+    with pytest.raises(ValueError):
+        output = NumberListManipulator([1])
+        output.max_adjacent
+    with pytest.raises(ValueError):
+        output = NumberListManipulator([])
+        output.max_adjacent
     return
--- test_sum_function.py
+++ test_sum_function.py
@@ -1,21 +1,21 @@
 def test_sum_function():
     import pytest
-    from number_manipulation import return_sum
-    case_one = return_sum([5])
-    case_two = return_sum([3, 7, -2])
-    case_three = return_sum([-6, -90, -40, 26])
-    case_four = return_sum([6.9, 3.2, -4])
-    case_five = return_sum([-3.2, -1.8, -9.36, -872])
+    from number_manipulation import NumberListManipulator
+    case_one = NumberListManipulator([5, 0])
+    case_two = NumberListManipulator([3, 7, -2])
+    case_three = NumberListManipulator([-6, -90, -40, 26])
+    case_four = NumberListManipulator([6.9, 3.2, -4])
+    case_five = NumberListManipulator([-3.2, -1.8, -9.36, -872])
 
-    assert case_one == 5
-    assert case_two == 8
-    assert case_three == -110
+    assert case_one.sum == 5
+    assert case_two.sum == 8
+    assert case_three.sum == -110
     # vertikoff - setting tollerance threshold here
     tollerance = 0.00000001
-    assert case_four - 6.1 < tollerance
-    assert case_five + 886.36 < tollerance
+    assert case_four.sum - 6.1 < tollerance
+    assert case_five.sum + 886.36 < tollerance
 
     with pytest.raises(TypeError):
-        return_sum(8, "str")
+        NumberListManipulator([8, "str"])
     with pytest.raises(ValueError):
-        return_sum([])
+        NumberListManipulator([])
