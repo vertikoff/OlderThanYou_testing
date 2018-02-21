82c83f2c7dc439d03883716d07def4c30fbc6f83
Cole Vertikoff <cole.vertikoff@gmail.com>
2018-02-20 19:15:19 -0500

    PEP8 fix

--- test_adjacent_diff_function.py
+++ test_adjacent_diff_function.py
@@ -2,7 +2,8 @@ def test_max_adjacent():
     import pytest
     from number_manipulation import NumberListManipulator
     assert NumberListManipulator([1, 2]).max_adjacent == 1
-    assert NumberListManipulator([0, 1, 3, 4, 5, 6, 7, 8, 9, 10]).max_adjacent == 2
+    assert NumberListManipulator([0, 1, 3, 4, 5, 6,
+                                  7, 8, 9, 10]).max_adjacent == 2
     assert NumberListManipulator([1, 2, 3]).max_adjacent == 1
     assert NumberListManipulator([1, 2, 2.5, 5]).max_adjacent == 2.5
     assert NumberListManipulator([-1, 1, -2]).max_adjacent == 3
