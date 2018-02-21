ef75b87adb1772b0c9d7a54b7048e630a4bd65a2
Cole Vertikoff <cole.vertikoff@gmail.com>
2018-02-21 14:24:30 -0500

    fixes indentation issue with Class level docsctrings

--- number_manipulation.py
+++ number_manipulation.py
@@ -1,13 +1,13 @@
 class NumberListManipulator:
-"""
+    """
 
-Determines, sum, max, min, and max adjacent diff for given list
-:param num_list: list of ints and floats
-:attr num_list: list passed by user
-:attr sum: sum of num_list
-:attr limits: a tuple in the form (minimum_value maximum_value)
-:attr max_adjacent: max diff between two adjacent members of list
-"""
+    Determines, sum, max, min, and max adjacent diff for given list
+    :param num_list: list of ints and floats
+    :attr num_list: list passed by user
+    :attr sum: sum of num_list
+    :attr limits: a tuple in the form (minimum_value maximum_value)
+    :attr max_adjacent: max diff between two adjacent members of list
+    """
 
     def __init__(self, num_list=None):
         self.num_list = num_list
